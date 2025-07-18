16e9cdcf8286dc668cb215331d12942d22559f99
Ckohler90 <123606446+Ckohler90@users.noreply.github.com>
2025-07-17 21:30:16 -0600

    still trying to deploy

--- next.config.cjs
+++ next.config.cjs
@@ -6,8 +6,8 @@ const nextConfig = {
     unoptimized: true
   },
   // Handle GitHub Pages subdomain routing if your repo isn't your-username.github.io
-  basePath: process.env.NODE_ENV === 'production' ? '/Ckohler90.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Ckohler90.github.io' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/lijitpixelbuilder' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/lijitpixelbuilder' : '',
 };
 
 export default nextConfig;
