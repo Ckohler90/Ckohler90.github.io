b837147cafecd39907eb30a0f592cbe710aef078
Ckohler90 <123606446+Ckohler90@users.noreply.github.com>
2025-07-17 21:12:02 -0600

    hold onto your butts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Handle GitHub Pages subdomain routing if your repo isn't your-username.github.io
+  basePath: process.env.NODE_ENV === 'production' ? '/Ckohler90.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Ckohler90.github.io' : '',
+};
 
 export default nextConfig;
